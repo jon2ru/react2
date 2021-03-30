e1d51b6ad66fd57f1fd8dc34ebf2c2f98da8b58e
Roman Tab <dekses@rambler.ru>
2021-03-30 22:11:52 +0300

    dobAvil files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/Header";
+import Nav from "./components/Nav";
+import Profile from "./components/Profile";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Nav />
+      <Profile />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,20 @@
+const Profile = () => {
+  return (
+    <div className="profile">
+      <img
+        src="https://i.pinimg.com/originals/f5/39/b6/f539b6967cb0f250d4e05cc133a8c87d.jpg"
+        width="800"
+        height="400"
+      />
+      <div>avat+descr</div>
+      <div>
+        My Post
+        <div>New Post</div>
+      </div>
+      <div>Post1</div>
+      <div>Post2</div>
+    </div>
+  );
+};
+
+export default Profile;
