ac095266558b2ddab4d3e2d0a965ac2f93a67241
Roman Tab <dekses@rambler.ru>
2021-04-07 21:44:08 +0300

    начинаю делать массивы в Dialogs and Myposts lesson number 24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -11,18 +11,26 @@ const Messages = (props) => {
   return <div className={classes.messages}>{props.message}</div>;
 };
 const Dialogs = (props) => {
+  let dialogsData = [
+    { id: 1, name: "Dimich" },
+    { id: 2, name: "Roma" },
+    { id: 3, name: "Petiya" },
+    { id: 4, name: "Igor" },
+  ];
+  let messagesData = [
+    { id: 1, message: "привет" },
+    { id: 2, message: "позвони мне" },
+    { id: 3, message: "напиши номер" },
+  ];
   return (
     <div className={classes.dialogs}>
       <div className={classes.dialogsitem}>
-        <Dialog name="Dimich" id="1" />
-        <Dialog name="Roma" id="2" />
-        <Dialog name="Petiya" id="3" />
-        <Dialog name="Igor" id="4" />
+        <Dialog name={dialogsData[0].name} id={dialogsData[0].id} />
+        <Dialog name={dialogsData[1].name} id={dialogsData[1].id} />
       </div>
       <div className={classes.dialog}>
-        <Messages message="привет" />
-        <Messages message="позвони мне" />
-        <Messages message="напиши номер" />
+        <Messages message={messagesData[0].message} />
+        <Messages message={messagesData[1].message} />
       </div>
     </div>
   );
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,6 +2,10 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 // console.log(MyPosts);
 const MyPosts = () => {
+  let postData = [
+    { id: 1, message: "Как дела", count: 2 },
+    { id: 2, message: "нормально", count: 5 },
+  ];
   return (
     <div className={classes.post}>
       <h2>My Post</h2>
@@ -14,8 +18,8 @@ const MyPosts = () => {
         </div>
       </div>
       <div className={classes.newpost}>
-     <Post message="Как дела?" count={2} />
-     <Post message="нормально"count= {3}/>
+     <Post message={postData[0].message} count={postData[0].count}/>
+     <Post message={postData[1].message} count={postData[1].count}/>
        </div>
     </div>
   );
