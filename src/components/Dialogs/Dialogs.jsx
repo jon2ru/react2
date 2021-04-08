0c4ea3eb2e7f6a054d90c07972c9e843cbe8665a
Roman Tab <dekses@rambler.ru>
2021-04-08 23:29:58 +0300

    lerning arrays method map in Dialogs and Myposts lesson number 24

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -11,27 +11,26 @@ const Messages = (props) => {
   return <div className={classes.messages}>{props.message}</div>;
 };
 const Dialogs = (props) => {
-  let dialogsData = [
+  let dialogs2 = [
     { id: 1, name: "Dimich" },
     { id: 2, name: "Roma" },
     { id: 3, name: "Petiya" },
     { id: 4, name: "Igor" },
   ];
-  let messagesData = [
+  let messages2 = [
     { id: 1, message: "привет" },
     { id: 2, message: "позвони мне" },
     { id: 3, message: "напиши номер" },
   ];
+  let dialogsElements = dialogs2.map((d) => 
+    <Dialog name={d.name} id={d.id} />
+  );
+  let messageElements = messages2.map((m) => <Messages message={m.message} />);
+
   return (
     <div className={classes.dialogs}>
-      <div className={classes.dialogsitem}>
-        <Dialog name={dialogsData[0].name} id={dialogsData[0].id} />
-        <Dialog name={dialogsData[1].name} id={dialogsData[1].id} />
-      </div>
-      <div className={classes.dialog}>
-        <Messages message={messagesData[0].message} />
-        <Messages message={messagesData[1].message} />
-      </div>
+      <div className={classes.dialogsitem}>{dialogsElements}</div>
+      <div className={classes.dialog}>{messageElements}</div>
     </div>
   );
 };
--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -2,25 +2,27 @@ import classes from "./MyPosts.module.css";
 import Post from "./Post/Post";
 // console.log(MyPosts);
 const MyPosts = () => {
-  let postData = [
+  let post = [
     { id: 1, message: "Как дела", count: 2 },
     { id: 2, message: "нормально", count: 5 },
   ];
+  let postElement = post.map((p) => (
+    <Post message={p.message} count={p.count} />
+  ));
   return (
     <div className={classes.post}>
       <h2>My Post</h2>
       <div>
         <div>
-        <textarea></textarea>
+          <textarea></textarea>
         </div>
         <div>
-        <button>Add post</button>
+          <button>Add post</button>
         </div>
       </div>
       <div className={classes.newpost}>
-     <Post message={postData[0].message} count={postData[0].count}/>
-     <Post message={postData[1].message} count={postData[1].count}/>
-       </div>
+      {postElement}
+      </div>
     </div>
   );
 };
