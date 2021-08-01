6d3119c8e30741b1297313e672220d54af1a9beb
Roman Tab <dekses@rambler.ru>
2021-08-01 22:59:39 +0300

    added componentDidMount in users  lesson number 54

--- src/Redux/usersReduser.js
+++ src/Redux/usersReduser.js
@@ -2,7 +2,7 @@ const FOLLOW = "FOLLOW";
 const UNFOLLOW = "UNFOLLOW";
 const SET_USERS = "SET_USERS";
 let initialState = {
-  users:[],
+  users: [],
 };
 
 const usersReduser = (state = initialState, action) => {
@@ -10,40 +10,41 @@ const usersReduser = (state = initialState, action) => {
     case FOLLOW:
       return {
         ...state,
-        users: state.users.map(u=> {
-          if (u.id === action.userId){
-          return { 
-            ...u,
-            followed: true
-           }
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return {
+              ...u,
+              followed: true,
+            };
           }
           return u;
-                                    })
-      }
+        }),
+      };
     case UNFOLLOW:
       return {
         ...state,
-        users: state.users.map(u=> {
-          if (u.id === action.userId){
-          return {
-             ...u,
-            followed: false
-           }
+        users: state.users.map((u) => {
+          if (u.id === action.userId) {
+            return {
+              ...u,
+              followed: false,
+            };
           }
           return u;
-                                    })
-      }
+        }),
+      };
 
-      case SET_USERS: 
-        return {
-          ...state,users: [...state.users, ...action.users]
-        }
-      
+    case SET_USERS:
+      return {
+        ...state,
+        users: [/*...state.users, появляются дубли*/ ...action.users],
+      };
+
     default:
       return state;
   }
 };
 export const followAC = (userId) => ({ type: FOLLOW, userId });
-export const unfollowAC = (userId) => ({type: UNFOLLOW, userId});
-export const setusersAC = (users) => ({type: SET_USERS, users});
+export const unfollowAC = (userId) => ({ type: UNFOLLOW, userId });
+export const setusersAC = (users) => ({ type: SET_USERS, users });
 export default usersReduser;
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -3,59 +3,68 @@ import styles from "./Users.module.css";
 import * as axios from "axios";
 import userPhoto from "../../assets/images/users.png";
 
-class Users extends React.Component{
-  constructor(props){
-  super(props);
-  axios
-      .get("https://social-network.samuraijs.com/api/1.0/users")
-      .then((ponse) => {
-        this.props.setUsers(ponse.data.items);
-      });
+class Users extends React.Component {
+ /* constructor(props) {
+    super(props);
+    axios
+       } если constructor ничего не делает и 
+       только отрисовывается через super, то его можно не писать*/
+  componentDidMount(){
+    axios
+    .get("https://social-network.samuraijs.com/api/1.0/users")
+    .then((response) => {
+      this.props.setUsers(response.data.items);
+    });
   }
   render() {
-  return  <div>
-    {this.props.users.map((u) => (
-      <div key={u.id}>
-        <span>
-          <div>
-            <img src={u.photos.small !== null ? u.photos.small : userPhoto } className={styles.userPhoto} />
+    return (
+      <div>
+        {this.props.users.map((u) => (
+          <div key={u.id}>
+            <span>
+              <div>
+                <img
+                  src={u.photos.small !== null ? u.photos.small : userPhoto}
+                  className={styles.userPhoto}
+                />
+              </div>
+              <div>
+                {
+                  u.followed ? (
+                    <button
+                      onClick={() => {
+                        this.props.follow(u.id);
+                      }}
+                    >
+                      Follow
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => {
+                        this.props.unfollow(u.id);
+                      }}
+                    >
+                      Unfollow
+                    </button>
+                  )
+                  //тринарное выражение
+                }
+              </div>
+            </span>
+            <span>
+              <span>
+                <div>{u.name}</div>
+                <div>{u.status}</div>
+              </span>
+              <span>
+                <div>{"u.location.city"}</div>
+                <div>{"u.location.cantry"}</div>
+              </span>
+            </span>
           </div>
-          <div>
-            {
-              u.followed ? (
-                <button
-                  onClick={() => {
-                    this.props.follow(u.id);
-                  }}
-                >
-                  Follow
-                </button>
-              ) : (
-                <button
-                  onClick={() => {
-                    this.props.unfollow(u.id);
-                  }}
-                >
-                  Unfollow
-                </button>
-              )
-              //тринарное выражение
-            }
-          </div>
-        </span>
-        <span>
-          <span>
-            <div>{u.name}</div>
-            <div>{u.status}</div>
-          </span>
-          <span>
-            <div>{'u.location.city'}</div>
-            <div>{'u.location.cantry'}</div>
-          </span>
-        </span>
+        ))}
       </div>
-    ))}
-  </div>
+    );
   }
 }
 export default Users;
