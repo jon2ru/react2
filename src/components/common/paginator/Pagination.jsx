c7eb206ee85d82305adc71d9aa04baac520121f9
Roman Tabunov <dekses@rambler.ru>
2022-03-12 23:08:58 +0300

    93 finish

--- src/components/common/paginator/Pagination.jsx
+++ src/components/common/paginator/Pagination.jsx
@@ -1,8 +1,10 @@
 import React,{useState} from "react";
 import styles from "./Paginator.module.css"
 import cn from "classnames";
+//classnames Эта библиотека для простого условного объединения имен классов
 
-let Pagination = ({totalItemsCount,pageSize, currentPage,onPageChanged, portionSize=10}) => {
+let Pagination = ({totalItemsCount,pageSize, currentPage,onPageChanged,
+   portionSize=10}) => {
   let pagesCount = Math.ceil(totalItemsCount /pageSize);
   //страниц=сколько всего юзеров/сколько показывать юзеров
   let pages = [];
@@ -25,15 +27,25 @@ let Pagination = ({totalItemsCount,pageSize, currentPage,onPageChanged, portionS
     .filter(p=> p>= leftPortionPageNumber&& p <= rightPortionPageNumber)
     .map((p) => {
         return (
-          <span
-            className={currentPage === p && styles.pageNumber.selectedPage}
-            //currentPage=выделяю жирным текущую страницу
-            onClick={() => {
+          // <span
+          //   className={currentPage === p && styles.selectedPage}
+          //   //currentPage=выделяю жирным текущую страницу
+          //   onClick={() => {
+          //     onPageChanged(p);
+          //   }}
+          // >
+          //   {p}
+          // </span>
+          <span className={ cn( {
+            [styles.selectedPage] : currentPage === p },styles.pageNumber)}
+            // если currentPage != p , то styles.selectedPage не применять- это 
+            // библиотека classnames которая объединяет 2 класса selectedPage и pageNumber
+            // pageNumber делает рамку, а selectedPage выделяет жирным текущую страницу
+             //key={p}
+             onClick={(e)=>{
               onPageChanged(p);
-            }}
-          >
-            {p}
-          </span>
+             }}
+             >{p}</span>
         );
  } )}
  {portionCount >portionNumber&& <button onClick={() =>{
