689205e50a48499e2e8544e6f0ce9fa46b0c96ac
Roman Tabunov <dekses@rambler.ru>
2022-06-10 23:07:17 +0300

    113 formik start

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,32 +1,80 @@
-import React,{FC} from "react";
+import { Field, Form, Formik } from "formik";
+import React, { FC } from "react";
 import { UserType } from "../../types/types";
 import Pagination from "../common/paginator/Pagination";
 import User from "./User";
-type PropsType={
-  users:Array<UserType>
-  totalUsersCount:number
-  pageSize:number
- currentPage:number
- followingInProgress:number[]
-  onPageChanged:(pageNumber:number)=>void //функция не возвращает ничего
-  follow:(userId:number)=>void
-  unfollow:(userId:number)=>void
+type PropsType = {
+  users: Array<UserType>
+  totalUsersCount: number
+  pageSize: number
+  currentPage: number
+  followingInProgress: number[]
+  onPageChanged: (pageNumber: number) => void //функция не возвращает ничего
+  follow: (userId: number) => void
+  unfollow: (userId: number) => void
 
 }
-
-let Users:FC<PropsType> = ({ totalUsersCount, pageSize, currentPage, onPageChanged,users,
-   ...props }) => {
-  return (
-    <div>
+let Users: FC<PropsType> = ({ totalUsersCount, pageSize, currentPage, onPageChanged, users,
+  ...props }) => {
+  return <div>
+      <UserSearchForm />
       <Pagination onPageChanged={onPageChanged} currentPage={currentPage}
         pageSize={pageSize} totalItemsCount={totalUsersCount} />
-        <div>
-      {users.map((u:any) => <User user={u} key={u.id} 
-      followingInProgress={props.followingInProgress}
-        follow={props.follow} unfollow={props.unfollow} />
-      )}
+      <div>
+        {users.map((u: any) => <User user={u} key={u.id}
+          followingInProgress={props.followingInProgress}
+          follow={props.follow} unfollow={props.unfollow} />
+        )}
       </div>
     </div>
-      )
 }; //end Users
+const usersSearchFormValidate = (values: any) => {
+  const errors = {};
+  return errors;
+} 
+type userSerachFormType={
+  term:string
+}
+//выше валидатор
+const UserSearchForm = () => {
+  const submit=(values:userSerachFormType, { setSubmitting }:{ setSubmitting:(isSubmitting:boolean )=>void})=>{
+
+    setTimeout(() => {
+
+      alert(JSON.stringify(values, null, 2));
+
+      setSubmitting(false);
+
+    }, 400);
+  }
+
+  return <div>
+    <Formik
+
+      initialValues={{ term: ''}}
+
+      validate={usersSearchFormValidate}
+      onSubmit={submit}
+       >
+
+      {({ isSubmitting }) => (
+
+        <Form>
+
+          <Field type="text" name="term" />
+
+          <button type="submit" disabled={isSubmitting}>
+
+            Fiend
+
+          </button>
+
+        </Form>
+
+      )}
+
+    </Formik>
+
+  </div>
+}
 export default Users;
