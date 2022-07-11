147e0efccfe80d5a716ef3f75e0bba423d0d3409
Roman Tabunov <dekses@rambler.ru>
2022-07-11 22:55:40 +0300

    1/2  118

--- src/pages/Chat/ChatPage.tsx
+++ src/pages/Chat/ChatPage.tsx
@@ -1,17 +1,49 @@
+import { message } from "antd"
+import { useEffect } from "react"
+
+ const ws=new WebSocket('wss://social-network.samuraijs.com/handlers/ChatHandler.ashx')
+ export type ChatMessageType={
+     message: string,
+     photo: string,
+     userId: number,
+     userName: string
+   }
  const ChatPage:React.FC=()=>{
    return  <div>
         <Chat />
     </div>
 }
  const Chat:React.FC=()=>{
+  useEffect(()=>{
+  ws.addEventListener('message',(e)=>{
+    console.log(JSON.parse(e.data));
+  })
+  },[])
    return  <div>
       <Messages2 />
       <AddMessssageform />
     </div>
 }
 const Messages2:React.FC=()=>{
+    const messages=[1,2,3,4]
+    return  <div style={{height:'400px',overflow: 'auto'}}>
+      { messages.map((m:any)=><Message />)} 
+      { messages.map((m:any)=><Message />)} 
+      { messages.map((m:any)=><Message />)} 
+     </div>
+ }
+const Message:React.FC=()=>{
+   const  message:ChatMessageType=null
+  //  {
+  //   url:'https://www.fauzer.ru/wa-data/public/shop/products/04/68/46804/images/60587/60587.70.jpg',
+  //   autor:'Petr',
+  //   text:'Hello!!!'
+  //  }
     return  <div>
-       message
+      <img src={message.url}/> <b>{message.autor}</b>
+      <br/>
+      {message.text}
+      <hr/>
      </div>
  }
  const AddMessssageform:React.FC=()=>{
